371268371803af0ddb4b5f6bd35802c4bb23959c
Mario <mario082@gmail.com>
2020-06-23 06:57:23 -0300

    introduçao

---
+++ sintaxe-variáveis-e-fluxo/src/Fatorial.java
@@ -0,0 +1,11 @@
+
+public class Fatorial {
+	public static void main(String[] args) {
+	int n = 6;
+		for (int i = n - 1; i >= 1; i--) {
+			n = n * i;
+		}
+		System.out.println(n);
+			
+	}
+}
---
+++ sintaxe-variáveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,17 @@
+
+public class TestaVariaveis {
+	public static void main(String[] args) {
+		System.out.println("Olá novo teste");
+		
+		int idade;
+		idade = 37;
+		
+		System.out.println(idade);
+		
+		idade = 30 + 10;
+		
+		System.out.println(idade);
+		
+		System.out.println("a idade é " + idade);
+	}
+}
